cacba45aec30891a135b81c50e413599f6115063
Nihal Vishal Gupta <nihalng786@gmail.com>
2023-10-13 18:55:06 +0530

    prisma setup done added all tables

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-
-import router from "./routes/testRoute.js";
+import initRoutes from "./routes/routes.js";
+import { PrismaClient } from "@prisma/client";
 
 dotenv.config();
 const PORT = process.env.PORT || 2000;
+
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(cors());
@@ -14,11 +16,13 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-// Routes
-app.use("/api/v1", router);
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
-
+try {
+  prisma.$connect();
+  console.log("Connected to database");
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+  initRoutes(app);
+} catch (error) {
+  console.log("Database not connected...");
+}
